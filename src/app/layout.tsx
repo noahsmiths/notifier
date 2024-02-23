f8777b339993d37f2e9eabb20dec0f58d1afa3c6
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2024-02-23 02:46:46 -0500

    Reformat to use 4-space tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Notifier",
-  description: "A small web app to enable mass pushing of data in live time to many users at once (like a classroom setting).",
+    title: "Notifier",
+    description: "A small web app to enable mass pushing of data in live time to many users at once (like a classroom setting).",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" data-theme="dark">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en" data-theme="dark">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <div className="btn">
-      Hello World
-    </div>
-  );
+    return (
+        <div className="btn">
+            Hello World
+        </div>
+    );
 }
